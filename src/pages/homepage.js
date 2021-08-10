a767e6a4f42cc25b794fe41f2fbe3717ea815482
KaMa75 <karolmarszalek@o2.pl>
2021-08-10 22:29:19 +0200

    styling directory menu on homepage

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -5,20 +5,20 @@ import './homepage.scss';
 import { menuItems } from "../data/menu-items";
 
 const HomePage = () => (
-    <>
-        {menuItems.map(menuItem => (
-            <div className='homepage' key={menuItem.id}>
-                <div className='directory-menu'>
-                    <div className='menu-item'>
-                        <div className='content'>
-                            <h1 className='title'>{menuItem.title}</h1>
-                            <span className='subtitle'>{menuItem.subtitle}</span>
-                        </div>
+
+    <div className='homepage'>
+        <div className='directory-menu'>
+            {menuItems.map(menuItem => (
+                <div className='menu-item' key={menuItem.id}>
+                    <div className='content'>
+                        <h1 className='title'>{menuItem.title}</h1>
+                        <span className='subtitle'>{menuItem.subtitle}</span>
                     </div>
                 </div>
-            </div>
-        ))}
-    </>
+            ))}
+        </div>
+    </div>
+
 );
 
 export default HomePage;
