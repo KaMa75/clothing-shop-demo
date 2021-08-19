9cb6c7f572527c8703ec971253f02932949c3fad
KaMa75 <karolmarszalek@o2.pl>
2021-08-19 21:36:34 +0200

    Directory menu rwd

--- src/components/menuItem/menuItem.js
+++ src/components/menuItem/menuItem.js
@@ -7,10 +7,13 @@ const MenuItem = ({title, subtitle, imageUrl, size}) => {
     return (
         <div
             className={`menu-item ${size}`}
-            style={{
-                backgroundImage: `url(${imageUrl})`
-            }}
         >
+            <div
+                className='background-image'
+                style={{
+                    backgroundImage: `url(${imageUrl})`
+                }}
+            />
             <div className='content'>
                 <h1 className='title'>{title}</h1>
                 <span className='subtitle'>{subtitle}</span>
