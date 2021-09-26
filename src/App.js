9fabf738251befbaba5293cc084bfd399f3e061b
KaMa75 <karolmarszalek@o2.pl>
2021-09-26 18:28:25 +0200

    add redirect form singin page when user login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { HomePage, HatsPage, ShopPage, SignInPage } from './pages';
@@ -46,7 +46,7 @@ class App extends Component {
             <Route exact path='/' component={HomePage} />
             <Route exact path='/hats' component={HatsPage} />
             <Route exact path='/shop' component={ShopPage} />
-            <Route exact path='/signin' component={SignInPage} />
+            <Route exact path='/signin' render={()=> this.props.currentUser ? (<Redirect to='/' />) : (<SignInPage />)} />
           </Switch>
         </div>
 
@@ -54,8 +54,12 @@ class App extends Component {
   }
 }
 
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
-})
+});
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
